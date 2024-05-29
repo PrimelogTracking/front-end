975d7a3a1e0e8fc68c8dd6133c129ef826a883ad
PrimelogTracking <primelogtracking@gmail.com>
2024-05-29 17:35:14 -0300

    feat: remove token

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER ?? "http://localhost:3001/api";
-const API_CLIENT = process.env.NEXT_PUBLIC_API_CLIENT ?? "http://localhost:3000/api";
-const TOKEN = process.env.NEXT_PUBLIC_TOKEN ?? "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJvb3QiLCJyb2xlIjoiYWRtaW4iLCJ5ZWFyIjoyMDI0LCJpYXQiOjE3MTYxNTAzMTB9.CCR4m7AciGkL4L59Ea7stbMy7_ltkMSOwNHGVpMnIxs";
+const API_SERVER = process.env.NEXT_PUBLIC_API_SERVER;
+const API_CLIENT = process.env.NEXT_PUBLIC_API_CLIENT;
+const TOKEN = process.env.NEXT_PUBLIC_TOKEN;
 
 const apiServer = async () => {
 	const instance = axios.create({ baseURL: API_SERVER });
